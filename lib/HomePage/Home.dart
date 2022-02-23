060dfb7a25057be4b7a85a46bfa7a3d35eb51601
NikhilJeikar <nikhiljeikar@gmail.com>
2022-02-23 13:00:52 +0530

    1.0

--- lib/HomePage/Home.dart
+++ lib/HomePage/Home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:library_management/HomePage/News/news.dart';
-import 'package:flutter_search_bar/flutter_search_bar.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 
 class Home extends StatefulWidget {
   String id, username;
@@ -16,13 +16,29 @@ class _HomeState extends State<Home> {
       onWillPop: () async => false,
       child: DefaultTabController(
           length: 2,
-          initialIndex: 1,
+          initialIndex: 0,
           child: Scaffold(
             body: TabBarView(
-              children: [Container(),news(id: widget.id)],
+              children: [Container(), news(id: widget.id)],
             ),
             appBar: AppBar(
                 automaticallyImplyLeading: false,
+                actions: [
+                  SizedBox(
+                    width: 200,
+                    height: 50,
+                    child: DropdownSearch(
+                      dialogMaxWidth: 100.0,
+                      dropdownSearchDecoration: const InputDecoration(
+                        hintText: "Select a country",
+                        labelText: "Menu mode *",
+                        contentPadding: EdgeInsets.fromLTRB(12, 12, 0, 0),
+                        border: OutlineInputBorder(),
+                      ),
+                      mode: Mode.MENU,
+                    ),
+                  )
+                ],
                 bottom: const TabBar(
                   tabs: [Icon(Icons.home), Icon(Icons.newspaper)],
                 )),
