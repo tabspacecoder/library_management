3502e7fd556ee38865dd66444a9fbdea068aa333
Mugunth J C <jc.mugunth@gmail.com>
2022-05-14 16:03:07 +0530

    circulation control mine

--- lib/Navbar/navbar.dart
+++ lib/Navbar/navbar.dart
@@ -58,8 +58,12 @@ class _NavBarState extends State<NavBar> {
     final channel = WebSocketChannel.connect(webSocket());
 
     channel.sink.add(parser(packet(widget.id, Handler.Handler1, Update.Password,
-        password: sha512.convert(utf8.encode(oldPassController.text.toString().trim())).toString(),
-        misc: sha512.convert(utf8.encode(newPassController.text.toString().trim())).toString())));
+        password: sha512
+            .convert(utf8.encode(oldPassController.text.toString().trim()))
+            .toString(),
+        misc: sha512
+            .convert(utf8.encode(newPassController.text.toString().trim()))
+            .toString())));
     channel.stream.listen((event) {
       event = event.split(Header.Split)[1];
       event = jsonDecode(event);
@@ -81,13 +85,34 @@ class _NavBarState extends State<NavBar> {
         bookName: BookName, username: UserName, author: [Author])));
     channel.sink.close();
   }
+
   var oldPassController = TextEditingController();
   var newPassController = TextEditingController();
   var confirmPassController = TextEditingController();
 
+  var UsernameController = TextEditingController();
+  var BookNameController = TextEditingController();
+  var DueDateController = TextEditingController();
+  String IssueDate = "${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}";
+  DateTime selectedDate = DateTime.now();
+  _selectDate (BuildContext context) async {
+    final DateTime? selected = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2010),
+      lastDate: DateTime(2025),
+    );
+    if (selected != null && selected != selectedDate)
+      setState(() {
+        selectedDate = selected;
+      });
+    setState(() {});
+  }
+
   bool online_avail = false;
   bool offline_avail = false;
   String dropdownvalue = 'Offline';
+  String DueDate = '';
   var items = [
     'Online',
     'Offline',
@@ -96,6 +121,88 @@ class _NavBarState extends State<NavBar> {
 
   late Uint8List pickedFileByteStream;
   late PlatformFile objFile;
+  void selectedItemCirculation(BuildContext context, int item) {
+    switch (item) {
+      case 1:
+        print('View Profile');
+        setState(() {});
+        break;
+      case 0:
+        showDialog(builder: (BuildContext context) {
+          return StatefulBuilder(builder: (BuildContext context,
+              void Function(void Function()) setState) {
+            return AlertDialog(
+              title:  Text('New Book Borrowal'),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    DueDate = "${selectedDate.day}/${selectedDate.month}/${selectedDate.year}";
+                    print('Book Name - ${BookNameController.text}');
+                    print('Username - ${UsernameController.text}');
+                    print('Issue Date - $IssueDate');
+                    print('Due date - $DueDate');
+                    setState(() {});
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Add record'),
+                ),
+              ],
+              scrollable: true,
+              content: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        controller: BookNameController,
+                        decoration: const InputDecoration(
+                          labelText: 'Enter Book name',
+                          icon: Icon(Icons.book),
+                        ),
+                      ),
+                      TextFormField(
+                        controller: UsernameController,
+                        decoration: const InputDecoration(
+                          labelText: 'Enter username',
+                          icon: Icon(Icons.account_circle),
+                        ),
+                      ),
+                      ListTile(
+                        title: Text('Due Date'),
+                        subtitle: Text('${selectedDate.day}/${selectedDate.month}/${selectedDate.year}'),
+                        leading: IconButton(icon:Icon(Icons.calendar_today), onPressed: () {_selectDate(context);},),
+                      ),
+                      // TextFormField(
+                      //   controller: DueDateController,
+                      //   decoration: InputDecoration(
+                      //     labelText: 'Due Date',
+                      //     icon: IconButton(icon:Icon(Icons.calendar_today), onPressed: () {_selectDate(context);},),
+                      //   ),
+                      // ),
+                    ],
+                  ),
+                ),
+              ),
+
+            );
+          }) ; }, context: context);
+        setState(() {});
+        break;
+      case 2:
+        print('logout');
+
+        break;
+
+      case 3:
+        print('My requests');
+        break;
+
+    }
+  }
   ListView userListView(context) {
     return ListView(
       // Remove padding
@@ -104,13 +211,12 @@ class _NavBarState extends State<NavBar> {
         UserAccountsDrawerHeader(
           accountName: Text(widget.username),
           accountEmail: Text(widget.curStatus),
-          currentAccountPicture:CircleAvatar(
-            // backgroundImage:
-            // AssetImage('assets/${widget.username[0].toLowerCase()}.png'),
+          currentAccountPicture: CircleAvatar(
             backgroundImage:
-            AssetImage('assets/a.png'),
+            AssetImage('assets/${widget.username[0].toLowerCase()}.png'),
+            // backgroundImage: AssetImage('assets/a.png'),
             radius: 30,
-          ) ,
+          ),
           decoration: const BoxDecoration(
             color: Colors.blue,
           ),
@@ -250,73 +356,78 @@ class _NavBarState extends State<NavBar> {
           leading: const Icon(Icons.password),
           title: const Text("Change Password"),
           onTap: () {
-            showDialog(builder: (BuildContext context) {
-              return StatefulBuilder(builder: (BuildContext context,
-                  void Function(void Function()) setState) {
-                return AlertDialog(
-                  title: const Text('Change Password'),
-                  actions: [
-                    TextButton(
-                      onPressed: () => Navigator.pop(context),
-                      child: const Text('Cancel'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                        if(newPassController.text != confirmPassController.text){
-                          showSnackbar(context, "New and Confirm passwords don't match");
-                        }
-                        else{
-                          changePasswordAPI();
-                        }
-                      },
-                      child: const Text('Change Password'),
-                    ),
-                  ],
-                  scrollable: true,
-                  content: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Form(
-                      child: Column(
-                        children: <Widget>[
-                          TextFormField(
-                            controller: oldPassController,
-                            obscureText: true,
-                            decoration: const InputDecoration(
-                              labelText: 'Old Password',
-                              icon: Icon(Icons.password_rounded),
-                            ),
+            showDialog(
+                builder: (BuildContext context) {
+                  return StatefulBuilder(builder: (BuildContext context,
+                      void Function(void Function()) setState) {
+                    return AlertDialog(
+                      title: const Text('Change Password'),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: const Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            if (newPassController.text !=
+                                confirmPassController.text) {
+                              showSnackbar(context,
+                                  "New and Confirm passwords don't match");
+                            } else {
+                              changePasswordAPI();
+                            }
+                          },
+                          child: const Text('Change Password'),
+                        ),
+                      ],
+                      scrollable: true,
+                      content: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Form(
+                          child: Column(
+                            children: <Widget>[
+                              TextFormField(
+                                controller: oldPassController,
+                                obscureText: true,
+                                decoration: const InputDecoration(
+                                  labelText: 'Old Password',
+                                  icon: Icon(Icons.password_rounded),
+                                ),
+                              ),
+                              TextFormField(
+                                controller: newPassController,
+                                obscureText: true,
+                                decoration: const InputDecoration(
+                                  labelText: 'New Password',
+                                  icon: Icon(Icons.fiber_new_rounded),
+                                ),
+                              ),
+                              TextFormField(
+                                controller: confirmPassController,
+                                obscureText: true,
+                                decoration: const InputDecoration(
+                                  labelText: 'Confirm Password',
+                                  icon: Icon(Icons.new_label),
+                                ),
+                              ),
+                            ],
                           ),
-                          TextFormField(
-                            controller: newPassController,
-                            obscureText: true,
-                            decoration: const InputDecoration(
-                              labelText: 'New Password',
-                              icon: Icon(Icons.fiber_new_rounded),
-                            ),
-                          ),
-                          TextFormField(
-                            controller: confirmPassController,
-                            obscureText: true,
-                            decoration: const InputDecoration(
-                              labelText: 'Confirm Password',
-                              icon: Icon(Icons.new_label),
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
-
-                );
-              }) ; }, context: context);
+                    );
+                  });
+                },
+                context: context);
             setState(() {});
           },
         ),
         ListTile(
             leading: const Icon(Icons.logout),
             title: const Text("Logout"),
-            onTap: () {}),
+            onTap: () {
+              Navigator.pushNamed(context, '/Login');
+            }),
       ],
     );
   }
@@ -340,14 +451,14 @@ class _NavBarState extends State<NavBar> {
               MaterialPageRoute(
                   builder: (context) => opacHome(
                         id: widget.id,
-                        title: 'OPAC',
+                        title: 'Search Book',
                       ))),
         ),
-        ListTile(
-          leading: Icon(Icons.all_out),
-          title: Text('Book Circulation'),
-          onTap: () => Navigator.pushNamed(context, '/Circulation'),
-        ),
+        // ListTile(
+        //   leading: Icon(Icons.all_out),
+        //   title: Text('Book Circulation'),
+        //   onTap: () => Navigator.pushNamed(context, '/Circulation'),
+        // ),
         ListTile(
           leading: Icon(Icons.add),
           title: Text('Add new book'),
@@ -513,11 +624,6 @@ class _NavBarState extends State<NavBar> {
           },
         ),
         ListTile(
-          leading: Icon(Icons.remove_red_eye_outlined),
-          title: Text('Search Book'),
-          onTap: () => null,
-        ),
-        ListTile(
           leading: Icon(Icons.notifications),
           title: Text('Pending Requests'),
           onTap: () => Navigator.push(
@@ -543,23 +649,76 @@ class _NavBarState extends State<NavBar> {
             ),
           ),
         ),
+        PopupMenuButton<int>(
+          child: ListTile(
+            leading: Icon(Icons.all_out),
+            title: Text('Book Circulation'),
+          ),
+          itemBuilder: (context) =>[
+            PopupMenuItem(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  title: Text('New Book Borrowal'),
+                  leading: CircleAvatar(
+                    child: Icon(Icons.add),
+                    radius: 30,
+                  ),
+                ),
+              ),
+              value: 0,
+            ),
+            PopupMenuItem(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  title: Text('Outstanding Books'),
+                  leading: CircleAvatar(
+                    child: Icon(Icons.add),
+                    radius: 30,
+                  ),
+                ),
+              ),
+              value: 1,
+            ),
+            PopupMenuItem(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  title: Text('Post due!'),
+                  leading: CircleAvatar(
+                    child: Icon(Icons.add),
+                    radius: 30,
+                  ),
+                ),
+              ),
+              value: 2,
+            ),
+            PopupMenuItem(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  title: Text('Book retreival'),
+                  leading: CircleAvatar(
+                    child: Icon(Icons.add),
+                    radius: 30,
+                  ),
+                ),
+              ),
+              value: 3,
+            ),
+          ],
+          onSelected: (item) {
+            selectedItemCirculation(context, item);
+          },
+        ),
         Divider(),
         ListTile(
-          leading: Icon(Icons.settings),
-          title: Text('Settings'),
-          onTap: () => null,
-        ),
-        ListTile(
-          leading: Icon(Icons.description),
-          title: Text('Policies'),
-          onTap: () => null,
-        ),
-        Divider(),
-        ListTile(
-          title: Text('Exit'),
-          leading: Icon(Icons.exit_to_app),
-          onTap: () => null,
-        ),
+            leading: const Icon(Icons.logout),
+            title: const Text("Logout"),
+            onTap: () {
+              Navigator.pushNamed(context, "/Login",arguments: widget.username);
+            }),
       ],
     );
   }
