dc7d9bfa58d86c54d075ae32b567f0f66e2ccea9
Mugunth J C <jc.mugunth@gmail.com>
2022-05-15 10:48:55 +0530

    combined and merged admin and user

--- lib/HomePage/Home.dart
+++ lib/HomePage/Home.dart
@@ -25,6 +25,7 @@ class _HomeState extends State<Home> {
     super.initState();
   }
 
+
   void GetState() async {
     final prefs = await SharedPreferences.getInstance();
     id = prefs.getString("Id")!;
@@ -40,6 +41,7 @@ class _HomeState extends State<Home> {
       curStatus = UserStat.User;
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
